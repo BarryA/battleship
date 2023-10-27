71c6108b0b8ab43f57dde1c5c18927a353fe5919
Sam Puttman <daputter2@gmail.com>
2023-10-26 20:33:41 -0400

    Add functionality to cell and ship as well as tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,1 +1,24 @@
-require './lib/cell.rb'
+require './lib/ship'
+require './lib/cell'
+
+RSpec.describe Cell do
+  
+  it "can initialize" do
+    cell = Cell.new("B4")
+
+    expect(cell).to be_an_instance_of(Cell)
+  end
+
+  it "has a name" do
+    cell = Cell.new("B4")
+
+    expect(cell.name).to eq("B4")
+  end
+
+  it "has a coordiante" do
+    cell = Cell.new("B4")
+
+    expect(cell.coordinate).to eq("B4")
+  end
+  
+end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,1 +1,52 @@
-require './lib/ship.rb'
+require './lib/ship'
+require 'pry'
+
+RSpec.describe Ship do
+  
+  it "can initialize" do
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser).to be_an_instance_of(Ship)
+  end
+
+  it "has a name" do
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser.name).to eq("Cruiser")
+  end
+
+  it "has a given length" do
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser.length).to eq(3)
+  end
+
+  it "has health" do
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser.health).to eq(3)
+  end
+
+  it "hasn't sunk yet" do
+    cruiser = Ship.new("Cruiser", 3)
+  
+    expect(cruiser.sunk?).to eq(false)
+  end
+
+  it "can be hit" do
+    cruiser = Ship.new("Cruiser", 3)
+    cruiser.hit
+
+    expect(cruiser.health).to eq(2)
+  end
+
+  it "can be sunk" do
+    cruiser = Ship.new("Cruiser", 3)
+    cruiser.hit
+    cruiser.hit
+    cruiser.hit
+
+    expect(cruiser.health).to eq(0)
+    expect(cruiser.sunk?).to eq(true)
+  end
+end
