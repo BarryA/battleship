4a3e0e36de30d2a46837d91c9305aa3aac4e0b9e
Sam Puttman <daputter2@gmail.com>
2023-10-27 16:16:20 -0400

    add additional methods

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,10 +1,29 @@
 class Cell
 
-  attr_reader :name, :coordinate
+  attr_reader :name, :coordinate, :ship
 
   def initialize(cell_name)
     @name = cell_name
     @coordinate = cell_name
+    @ship = nil
+    @fired_upon = false
   end
-  
+
+  def empty?
+    @ship.nil?
+  end
+
+  def place_ship(ship)
+    @ship = ship
+  end
+
+  def fired_upon
+    
+  end
+
+  def render
+
+  end
+
+
 end
