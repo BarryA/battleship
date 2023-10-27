1b66d433ebeb4c0e0ffab91d15c38528247aeaf6
Sam Puttman <daputter2@gmail.com>
2023-10-27 11:01:29 -0400

    add additional tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -21,4 +21,58 @@ RSpec.describe Cell do
     expect(cell.coordinate).to eq("B4")
   end
   
+  describe "#empty?" do
+    it "returns true for an empty cell" do
+      cell = Cell.new("B4")
+      
+      expect(cell.empty?).to be true
+    end
+  end
+
+  it "returns false for a cell with a ship" do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    
+    expect(cell.empty?).to be false
+  end
+end
+
+describe "#place_ship" do
+  it "places a ship in the cell" do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    
+    expect(cell.ship).to eq(cruiser)
+  end
+end
+
+describe "#fired_upon?" do
+  it "returns false when a cell has not been fired upon" do
+    cell = Cell.new("B4")
+    
+    expect(cell.fired_upon?).to be false
+  end
+
+  it "returns true when a cell has been fired upon" do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    cell.fire_upon
+    
+    expect(cell.fired_upon?).to be true
+  end
+end
+
+describe "#fire_upon" do
+  it "damages the ship in the cell" do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    cell.fire_upon
+    
+    expect(cell.ship.health).to eq(2)
+  end
+
 end
