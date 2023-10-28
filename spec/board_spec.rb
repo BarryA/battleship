ad2a5b56057b9e727f085de00297f6be8b1f8a1f
Sam Puttman <daputter2@gmail.com>
2023-10-28 13:24:42 -0400

    change syntax to match in ship_spec and add tests to board_spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,16 +3,30 @@ require './lib/cell.rb'
 require './lib/ship.rb'
 require 'pry'
 
-describe "initialize" do
-  it "can initialize" do
-    board = Board.new
+RSpec.describe Board do
+  describe "initialize" do
+    it "can initialize" do
+      board = Board.new
 
-    expect(board).to be_an_instance_of(Board)
+      expect(board).to be_an_instance_of(Board)
+    end
+
+    it "initializes with cells" do
+      board = Board.new
+
+      expect(board.cells.length).to eq(16)
+    end
   end
 
-  it "initializes with cells" do
-    board = Board.new
+  describe "valid coordinates" do
+    it "can verify valid coordinates" do
+      board = Board.new
 
-    expect(board.cells.length).to eq(16)
+      expect(board.valid_coordinate?("A1")).to be true
+      expect(board.valid_coordinate?("D4")).to be true
+      expect(board.valid_coordinate?("A5")).to be false
+      expect(board.valid_coordinate?("E1")).to be false
+      expect(board.valid_coordinate?("A22")).to be false
+    end
   end
 end
