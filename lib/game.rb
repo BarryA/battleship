d7f68674f2b461324a1568dc26adeaa0ac4c04e3
Barry Allahyar <barrya@gmail.com>
2023-10-31 14:27:35 -0500

    refactor game.rb with player_ships and computer_ships

--- lib/game.rb
+++ lib/game.rb
@@ -6,19 +6,21 @@ class Game
 
   attr_reader :player_board,
               :computer_board,
-              :player_cruiser, 
-              :player_submarine, 
-              :computer_cruiser, 
-              :computer_submarine
+              :player_ships, 
+              :computer_ships
               
 
   def initialize
     @player_board = Board.new
     @computer_board = Board.new
-    @player_cruiser = Ship.new("Cruiser", 3)
-    @player_submarine = Ship.new("Submarine", 2)
-    @computer_cruiser = Ship.new("Cruiser", 3)
-    @computer_submarine = Ship.new("Submarine", 2)
+    @player_ships = [
+      Ship.new("Cruiser", 3),
+      Ship.new("Submarine", 2)
+      ]
+    @computer_ships = [
+      Ship.new("Cruiser", 3),
+      Ship.new("Submarine", 2)
+      ]
   end
 
   def main_menu
