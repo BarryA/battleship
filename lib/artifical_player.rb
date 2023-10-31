e022f062ae29bb87e169f3d83257c757cf7a5f06
Sam Puttman <daputter2@gmail.com>
2023-10-31 11:38:08 -0400

    add outline and start coding select_coordinate.

--- lib/artifical_player.rb
+++ lib/artifical_player.rb
@@ -1,4 +1,10 @@
+require 'lib/board'
+require 'lib/cell'
+require 'lib/ship'
+require 'pry'
+
 class ArtificialPlayer
+  
   attr_reader :enemy_board
 
 
@@ -9,6 +15,17 @@ class ArtificialPlayer
     @enemy_board = board
     @previous_shots = []
     @last_hit = nil
+  end
+
+  #if last target was a hit, utilize targeted_coordinates
+  def select_coordinate
+    if @last_hit
+      possible_coordinates = adjacent_coordinates(@last_hit)
+      # target possible coordinates that are not included in previous shots
+      # and a valid coordinate
+      # .find?
+      target = possible_coordinates.find { |coordinate| }
+    end
   end
 
 
@@ -29,9 +46,17 @@ class ArtificialPlayer
 
   # need to define coodinates around hits as coordinates to attack (later)
   def adjacent_coordinates(coordinate)
-    row = coordinate[0].ord
+    row = coordinate[0]
     column = coordinate[1].to_i
-    
+
+    targeted_coordinates = [
+      "#{row}#{column - 1}", "#{row}#{column + 1}",
+      "#{row.ord - 1}#{column}", "#{row.ord + 1}#{column}"
+    ]
+
+    # select randomly out of targeted_coordinates
+    # need to ensure that the coordinate is valid and not already targeted.
+    targeted_coordinates.select { |coordinate| @enemy_board.valid_coordinate?(coordinate) }
   end
 
 
